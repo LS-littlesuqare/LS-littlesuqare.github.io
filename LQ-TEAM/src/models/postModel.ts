7e835e2b92f5dab85162f5b10b87027eb21450a9
Franky yang <frankyyangjr@outlook.com>
2025-09-21 17:15:24 +0800

    Create postModel.ts

---
+++ LQ-TEAM/src/models/postModel.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+postSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const Post = model('Post', postSchema);
+
+export default Post;
