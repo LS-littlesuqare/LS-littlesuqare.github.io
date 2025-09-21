129a139620c0562185168f09cb6973f52e0b9bb3
Franky yang <frankyyangjr@outlook.com>
2025-09-21 17:14:16 +0800

    Create authController.ts

---
+++ LQ-TEAM/src/controllers/authController.ts
@@ -0,0 +1,11 @@
+class AuthController {
+    async register(req, res) {
+        // 处理用户注册请求的逻辑
+    }
+
+    async login(req, res) {
+        // 处理用户登录请求的逻辑
+    }
+}
+
+export default AuthController;
